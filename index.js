50cf176a1fabf53abf1698faca4f2818e3cf5366
jessef <1319957+jfrausto@users.noreply.github.com>
2020-08-27 00:12:45 -0700

    able to grab title from responses

--- index.js
+++ index.js
@@ -1,14 +1,79 @@
 var generateMarkdown = require("./utils/generateMarkdown");
 var fs = require("fs");
+var inquirer = require("inquirer");
 
 // array of questions for user
-const questions = [];
+// 0:title, 1:desc, 2:installation, 3:usage,
+// 4:license, 5:contributions, 6:tests, 7:username, 8:email
+const questions = [
+  "What is the title of your project?",
+  "Write a short description: ",
+  "How do you install this application? ",
+  "Usage information: ",
+  "Choose a license: ",
+  "What are some contribution guidelines? ",
+  "Test instructions: ",
+  "Enter your GitHub username: ",
+  "Enter your email: ",
+];
 
 // function to write README file
 function writeToFile(fileName, data) {}
-
 // function to initialize program
-function init() {}
+function init() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "title",
+        message: questions[0],
+      },
+      {
+        type: "input",
+        name: "description",
+        message: questions[1],
+      },
+      {
+        type: "input",
+        name: "installation",
+        message: questions[2],
+      },
+      {
+        type: "input",
+        name: "usage",
+        message: questions[3],
+      },
+      {
+        type: "list",
+        message: questions[4],
+        name: "license",
+        choices: ["MIT", "Apache 2.0", "AGPLv3"],
+      },
+      {
+        type: "input",
+        name: "contributions",
+        message: questions[5],
+      },
+      {
+        type: "input",
+        name: "testing",
+        message: questions[6],
+      },
+      {
+        type: "input",
+        name: "githubUser",
+        message: questions[7],
+      },
+      {
+        type: "input",
+        name: "email",
+        message: questions[8],
+      },
+    ])
+    .then(function (data) {
+      console.log(data.title);
+    });
+}
 
 // function call to initialize program
 init();
