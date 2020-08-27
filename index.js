d9f3a7f272bb5b17053c1d9542bbc45b3a10595d
jessef <1319957+jfrausto@users.noreply.github.com>
2020-08-26 23:41:01 -0700

    initial setup with packages and files

---
+++ index.js
@@ -0,0 +1,14 @@
+var generateMarkdown = require("./utils/generateMarkdown");
+var fs = require("fs");
+
+// array of questions for user
+const questions = [];
+
+// function to write README file
+function writeToFile(fileName, data) {}
+
+// function to initialize program
+function init() {}
+
+// function call to initialize program
+init();
