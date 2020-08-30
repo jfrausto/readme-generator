c5d025257cd8c1f501cf1d86836e7e0c6ce26fc9
jessef <1319957+jfrausto@users.noreply.github.com>
2020-08-30 14:21:33 -0700

    correct format of badges and new lines

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,12 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+
+  ${data.badge}
+
   ${data.description}
   ## Table of Contents
+
    - [Installation](#Installation)
    - [Usage](#Usage)
    - [License](#License)
@@ -10,16 +14,22 @@ function generateMarkdown(data) {
    - [Tests](#Testing)
    - [Questions](#Questions)
   ## Installation
+
   ${data.installation}
   ## Usage
+
   ${data.usage}
   ## License
-  ${data.license}
+
+  This project is covered under the **${data.license}** license -- see more info [here](${data.badgeDescUrl}).
   ## Contributing
+
   ${data.contributions}
   ## Tests
+
   ${data.testing}
   ## Questions
+
   - GitHub: [${data.githubUser}](https://github.com/${data.githubUser})
   - If you have any further questions, you can reach me at _${data.email}_
   `;
